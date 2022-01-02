82f818499295d2aca00d53e211632b428cb8c296
Joice Barbosa <joicevbarbosa@gmail.com>
2022-01-01 23:28:59 -0300

    Aulas_Js

--- Aula 4 - estruturas condicionais/scrip.js
+++ Aula 4 - estruturas condicionais/scrip.js
@@ -1,5 +1,5 @@
-var jogador1 = 1;
-var jogador2 = 1;
+/*var jogador1 = 0;
+var jogador2 = 0;
 var placar;
 
 //if ternário de uma linha só
@@ -20,4 +20,67 @@ if (jogador1 > 0 && jogador2 == 0) {
     console.log('Ninguem marcou ponto')
 }
 
-//paramos em 7:42min - USANDO SWITCH/CASE - switch case
+//Switch/case- funciona como estrutura condicional tambem
+switch (placar){
+    case placar = jogador1 > jogador2:
+        console.log('jogador 1 ganhou');
+        break;
+    case placar = jogador2 > jogador1:
+        console.log('jogador 2 ganhou');
+        break;
+    default: console.log('Ninguem ganhou')
+
+}*/
+
+//Laços de repetição - são estrutuas que repetem uma instrução até atingir determinada condição.
+let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5',];
+let object = { propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3', }
+
+//for - executa uma instrução ate que ela seja falsa:
+/*for (let indice = 0; indice < array.length; indice++) {
+    console.log(indice)    
+}*/
+
+// for/in - executa uma repetição a apartir de uma propriedade 
+// com array
+/* for (let i in array) {
+    console.log(i);
+}
+
+for (i in object) {
+    console.log(i)
+} */
+
+//for of - funciona como uma repetição a partir de uma valor
+/* for (i of array) {
+    console.log(i); 
+}
+
+for (i of object.propriedade1) {
+    console.log(i);
+
+} */
+
+//while - executa instrução "enquanto" determinada condição for verdadeira, a verificação é feita antes da execução ;
+
+var a = 0;
+while (a < 10){
+    a++;
+    console.log(a);
+} do {
+    a++;
+    console.log(a);
+    
+}while(a < 10)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Aula 6 - aprofundando em funções/tiposDeFucao.js
@@ -0,0 +1,22 @@
+//tipos de função
+
+//declarativas
+/* function funcao() {
+  console.log('sou uma mensagem vinda de uma função declarativa');  
+ 
+}
+funcao(); */
+
+//Expressão de função
+// com nomeação
+/* var funcao = function() {
+  console.log('Sou uma mensagem de função de expressão');
+}
+funcao(); */
+
+//arrow function
+var funcao = () => {
+    console.log('Sou uma arrow function');
+}
+    funcao();
+
